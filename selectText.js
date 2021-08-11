8f03c8fc8928e409c143393332979c8efec6b42a
bsides44 <maddog.star@gmail.com>
2021-08-12 09:55:46 +1200

    click events working

--- selectText.js
+++ selectText.js
@@ -1,12 +1,33 @@
+// AFRAME.registerComponent('selecttext', {
+//     init: function () {
+//         let el = this.el;
+//         this.doSomething = function () {
+//             console.log('clicked')
+//         };
+//         this.el.addEventListener('click', this.doSomething);
+//     },
+//     remove: function () {
+//         this.el.removeEventListener('click', this.doSomething);
+//     }
+// })
+
 AFRAME.registerComponent('selecttext', {
+
     init: function () {
-        let el = this.el;
-        this.doSomething = function () {
+        console.log('rolling')
+        const myMarker = document.querySelector("#heart-marker");
+        const aEntity = document.querySelector("#fetus-model");
+
+        // every click, we make our model grow in size :)
+        myMarker.addEventListener('click', function (ev, target) {
+            console.log('ev', ev)
             console.log('clicked')
-        };
-        this.el.addEventListener('click', this.doSomething);
-    },
-    remove: function () {
-        this.el.removeEventListener('click', this.doSomething);
+            const intersectedElement = ev && ev.detail && ev.detail.intersection && ev.detail.intersection.object;
+            if (intersectedElement) {
+                const scale = aEntity.getAttribute('scale');
+                Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
+                aEntity.setAttribute('scale', scale);
+            }
+        });
     }
-})
+});
